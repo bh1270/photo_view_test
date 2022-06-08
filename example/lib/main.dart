7954c37d0294140a0f5d8980640238984a02f45a
bh_iOS <hubei_0721@126.com>
2022-06-08 15:59:11 +0800

    function

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:digiwhale_photo_view/digiwhale_photo_view.dart';
-import 'package:photo_preview/photo_preview_export.dart';
 
 void main() {
   runApp(const MyApp());
@@ -74,30 +73,7 @@ class _MyAppState extends State<MyApp> {
               child: Text(_platformVersion),
             ),
             onTap: () {
-              PhotoPreviewPage.navigatorPush(
-                  context,
-                  PhotoPreviewDataSource(imgVideoFullList: [
-                    PhotoPreviewInfoVo(
-                        url: "https://s1.ax1x.com/2020/09/17/wR3WnI.jpg",
-                        loadingCoverUrl:
-                            "https://s1.ax1x.com/2020/09/17/wR3WnI.md.jpg",
-                        type: PhotoPreviewType.image),
-                    PhotoPreviewInfoVo(
-                        url: "https://v-cdn.zjol.com.cn/277001.mp4",
-                        loadingCoverUrl:
-                            "https://s1.ax1x.com/2020/09/17/wR8uCD.jpg",
-                        type: PhotoPreviewType.video),
-                    PhotoPreviewInfoVo(
-                        url: "https://s1.ax1x.com/2020/09/17/wR0NmF.jpg",
-                        loadingCoverUrl:
-                            "https://s1.ax1x.com/2020/09/17/wR0NmF.md.jpg",
-                        type: PhotoPreviewType.image),
-                    PhotoPreviewInfoVo(
-                        url: "https://s1.ax1x.com/2020/09/17/wR3H3Q.jpg",
-                        loadingCoverUrl:
-                            "https://s1.ax1x.com/2020/09/17/wR3H3Q.md.jpg",
-                        type: PhotoPreviewType.image)
-                  ]));
+                DigiwhalePhotoView().testPush(context);
             },
           ),
         ),
--- lib/digiwhale_photo_view.dart
+++ lib/digiwhale_photo_view.dart
@@ -1,7 +1,7 @@
-
 import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:photo_preview/photo_preview_export.dart';
 
 class DigiwhalePhotoView {
   static const MethodChannel _channel = MethodChannel('digiwhale_photo_view');
@@ -9,5 +9,28 @@ class DigiwhalePhotoView {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  testPush(BuildContext context) {
+    PhotoPreviewPage.navigatorPush(
+        context,
+        PhotoPreviewDataSource(imgVideoFullList: [
+          PhotoPreviewInfoVo(
+              url: "https://s1.ax1x.com/2020/09/17/wR3WnI.jpg",
+              loadingCoverUrl: "https://s1.ax1x.com/2020/09/17/wR3WnI.md.jpg",
+              type: PhotoPreviewType.image),
+          PhotoPreviewInfoVo(
+              url: "https://v-cdn.zjol.com.cn/277001.mp4",
+              loadingCoverUrl: "https://s1.ax1x.com/2020/09/17/wR8uCD.jpg",
+              type: PhotoPreviewType.video),
+          PhotoPreviewInfoVo(
+              url: "https://s1.ax1x.com/2020/09/17/wR0NmF.jpg",
+              loadingCoverUrl: "https://s1.ax1x.com/2020/09/17/wR0NmF.md.jpg",
+              type: PhotoPreviewType.image),
+          PhotoPreviewInfoVo(
+              url: "https://s1.ax1x.com/2020/09/17/wR3H3Q.jpg",
+              loadingCoverUrl: "https://s1.ax1x.com/2020/09/17/wR3H3Q.md.jpg",
+              type: PhotoPreviewType.image)
+        ]));
   }
 }
